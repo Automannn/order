be3eee0680262167eccd3ed39ea7c6c1bca4f150
Automannn <Automannn@163.com>
2019-07-28 11:06:24 +0800

    delploy参数化

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,8 @@
 # 项目路径, 在Execute Shell中配置项目路径, pwd 就可以获得该项目路径
 # export PROJ_PATH=这个jenkins任务在部署机器上的路径
 
+# export APP_NAME=这个jenkins任务在的名称
+
 # 输入你的环境上tomcat的全路径
 # export TOMCAT_APP_PATH=tomcat在部署机器上的路径
 
@@ -27,15 +29,17 @@ mvn clean install
 killTomcat
 
 # 删除原有工程
-rm -rf $TOMCAT_APP_PATH/webapps/ROOT
-rm -f $TOMCAT_APP_PATH/webapps/ROOT.war
-rm -f $TOMCAT_APP_PATH/webapps/order.war
+#rm -rf $TOMCAT_APP_PATH/webapps/ROOT
+#rm -f $TOMCAT_APP_PATH/webapps/ROOT.war
+#rm -f $TOMCAT_APP_PATH/webapps/order.war
+rm -f $TOMCAT_APP_PATH/webapps/$APP_NAME
+rm -f $TOMCAT_APP_PATH/webapps/"$APP_NAME".war
 
 # 复制新的工程
-cp $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/webapps/
+cp $PROJ_PATH/"$APP_NAME"/target/"$APP_NAME".war $TOMCAT_APP_PATH/webapps/
 
-cd $TOMCAT_APP_PATH/webapps/
-mv order.war ROOT.war
+#cd $TOMCAT_APP_PATH/webapps/
+#mv order.war ROOT.war
 
 # 启动Tomcat
 cd $TOMCAT_APP_PATH/
